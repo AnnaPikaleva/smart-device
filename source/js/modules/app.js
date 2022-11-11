679c6ec0a3b70561838c33c6236d5c6ddde67e96
AnnaPikaleva <97855987+AnnaPikaleva@users.noreply.github.com>
2022-11-11 22:16:30 +0300

    Правки после тестирования

--- source/js/modules/app.js
+++ source/js/modules/app.js
@@ -2,7 +2,8 @@ const moveToForm = () => {
   const buttonToForm = document.querySelector('.main-page__container > a');
   const form = document.querySelector('form');
 
-  buttonToForm.addEventListener('click', () => {
+  buttonToForm.addEventListener('click', function (event) {
+    event.preventDefault();
     form.scrollIntoView({
       block: 'nearest',
       behavior: 'smooth',
